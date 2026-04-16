617a2491e217d2e78ac69438a3f0eb53111297d5
shy-coding24 <salwahafizhahyus@gmail.com>
2026-04-16 10:49:43 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,32 @@
-console.log("JS Connected");
+const form = document.getElementById("expense-form");
+const nameInput = document.getElementById("name");
+const amountInput = document.getElementById("amount");
+const categoryInput = document.getElementById("category");
+const list = document.getElementById("list");
+
+form.addEventListener("submit", function(e) {
+  e.preventDefault();
+
+  const name = nameInput.value;
+  const amount = amountInput.value;
+  const category = categoryInput.value;
+
+  addTransaction(name, amount, category);
+
+  form.reset();
+});
+
+function addTransaction(name, amount, category) {
+  const li = document.createElement("li");
+
+  li.innerHTML = `
+    ${name} - Rp ${amount} (${category})
+    <button onclick="deleteTransaction(this)">X</button>
+  `;
+
+  list.appendChild(li);
+}
+
+function deleteTransaction(button) {
+  button.parentElement.remove();
+}
